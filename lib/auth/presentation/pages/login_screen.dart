2b194b69c8a556650b8ec663f4605eb750b37304
yousraamr <yousraamr000@gmail.com>
2025-07-22 21:51:33 +0200

    Update: wrapped color and textColor

--- lib/auth/presentation/pages/login_screen.dart
+++ lib/auth/presentation/pages/login_screen.dart
@@ -29,9 +29,9 @@ class _SignInScreenState extends State<SignInScreen> {
             flex: 7,
             child: Container(
               padding: const EdgeInsets.fromLTRB(25.0, 50.0, 25.0, 20.0),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
+              decoration: BoxDecoration(
+                color: lightColorScheme.onPrimary,
+                borderRadius: const BorderRadius.only(
                   topLeft: Radius.circular(40.0),
                   topRight: Radius.circular(40.0),
                 ),
@@ -63,18 +63,18 @@ class _SignInScreenState extends State<SignInScreen> {
                         decoration: InputDecoration(
                           label: const Text('Email'),
                           hintText: 'Enter Email',
-                          hintStyle: const TextStyle(
-                            color: Colors.black26,
+                          hintStyle: TextStyle(
+                            color: lightColorScheme.onBackground,
                           ),
                           border: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.black12, // Default border color
+                            borderSide: BorderSide(
+                              color: lightColorScheme.onBackground,
                             ),
                             borderRadius: BorderRadius.circular(10),
                           ),
                           enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.black12, // Default border color
+                            borderSide: BorderSide(
+                              color: lightColorScheme.onBackground,
                             ),
                             borderRadius: BorderRadius.circular(10),
                           ),
@@ -95,18 +95,18 @@ class _SignInScreenState extends State<SignInScreen> {
                         decoration: InputDecoration(
                           label: const Text('Password'),
                           hintText: 'Enter Password',
-                          hintStyle: const TextStyle(
-                            color: Colors.black26,
+                          hintStyle: TextStyle(
+                            color: lightColorScheme.onBackground,
                           ),
                           border: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.black12, // Default border color
+                            borderSide: BorderSide(
+                              color: lightColorScheme.onBackground,
                             ),
                             borderRadius: BorderRadius.circular(10),
                           ),
                           enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.black12, // Default border color
+                            borderSide: BorderSide(
+                              color: lightColorScheme.onBackground,
                             ),
                             borderRadius: BorderRadius.circular(10),
                           ),
@@ -129,10 +129,10 @@ class _SignInScreenState extends State<SignInScreen> {
                                 },
                                 activeColor: lightColorScheme.primary,
                               ),
-                              const Text(
+                               Text(
                                 'Remember me',
                                 style: TextStyle(
-                                  color: Colors.black45,
+                                  color: lightColorScheme.onBackground,
                                 ),
                               ),
                             ],
@@ -182,25 +182,25 @@ class _SignInScreenState extends State<SignInScreen> {
                           Expanded(
                             child: Divider(
                               thickness: 0.7,
-                              color: Colors.grey.withOpacity(0.5),
+                              color: lightColorScheme.outlineVariant.withOpacity(0.5),
                             ),
                           ),
-                          const Padding(
-                            padding: EdgeInsets.symmetric(
+                           Padding(
+                            padding: const EdgeInsets.symmetric(
                               vertical: 0,
                               horizontal: 10,
                             ),
                             child: Text(
                               'Sign up with',
                               style: TextStyle(
-                                color: Colors.black45,
+                                color: lightColorScheme.onBackground,
                               ),
                             ),
                           ),
                           Expanded(
                             child: Divider(
                               thickness: 0.7,
-                              color: Colors.grey.withOpacity(0.5),
+                              color: lightColorScheme.outlineVariant.withOpacity(0.5),
                             ),
                           ),
                         ],
@@ -215,10 +215,10 @@ class _SignInScreenState extends State<SignInScreen> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Text(
+                           Text(
                             'Don\'t have an account? ',
                             style: TextStyle(
-                              color: Colors.black45,
+                              color: lightColorScheme.onBackground,
                             ),
                           ),
                           GestureDetector(
