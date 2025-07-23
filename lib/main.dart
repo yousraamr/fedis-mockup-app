c19485c84b13fc3e8e86db514028c1282b3e02ea
yousraamr <yousraamr000@gmail.com>
2025-07-23 18:03:46 +0200

    Add: localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:fedis_mockup_demo/auth/presentation/pages/welcome_page.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:fedis_mockup_demo/themes/theme.dart';
+import 'package:fedis_mockup_demo/auth/presentation/pages/welcome_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'lib/translations',
+      fallbackLocale: const Locale('en'),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: lightMode,
+      darkTheme: darkMode,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: const WelcomeScreen(),
     );
   }
 }
-
