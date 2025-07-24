bd65c5bfca2645488171f51e8986b5bc520c9db5
yousraamr <yousraamr000@gmail.com>
2025-07-24 02:46:21 +0200

    Updated

--- lib/auth/presentation/pages/welcome_page.dart
+++ lib/auth/presentation/pages/welcome_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
 import 'package:fedis_mockup_demo/auth/presentation/pages/login_screen.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/signup_page.dart';
 import 'package:fedis_mockup_demo/themes/theme.dart';
 import 'package:fedis_mockup_demo/auth/presentation/widgets/custom_scaffold.dart';
 import 'package:fedis_mockup_demo/auth/presentation/widgets/welcome_button.dart';
+import 'package:fedis_mockup_demo/translations/welcome_page_strings.dart';
+
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -23,20 +27,19 @@ class WelcomeScreen extends StatelessWidget {
                 child: Center(
                   child: RichText(
                     textAlign: TextAlign.center,
-                    text: const TextSpan(
+                    text: TextSpan(
                       children: [
                         TextSpan(
-                            text: 'Welcome Back!\n',
-                            style: TextStyle(
-                              fontSize: 45.0,
-                              fontWeight: FontWeight.w600,
-                            )),
-                        TextSpan(
-                            text:
-                            '\nEnter personal details to your employee account',
+                          text: '${welcomeBack.tr()}\n',
+                            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                              color: lightColorScheme.onBackground,
+                            ),
+                        ),
+                         TextSpan(
+                            text: '\n${enterDetails.tr()}',
                             style: TextStyle(
                               fontSize: 20,
-                              // height: 0,
+                              color: lightColorScheme.primary,
                             ))
                       ],
                     ),
@@ -51,15 +54,15 @@ class WelcomeScreen extends StatelessWidget {
                 children: [
                    Expanded(
                     child: WelcomeButton(
-                      buttonText: 'Login',
+                      buttonText: login.tr(),
                       onTap: const SignInScreen(),
                       color: Colors.transparent,
-                      textColor: lightColorScheme.onPrimary,
+                      textColor: lightColorScheme.onBackground,
                     ),
                   ),
                   Expanded(
                     child: WelcomeButton(
-                      buttonText: 'Sign up',
+                      buttonText: signUp.tr(),
                       onTap: const SignUpScreen(),
                       color: lightColorScheme.onPrimary,
                       textColor: lightColorScheme.primary,
