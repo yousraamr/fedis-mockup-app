a1f90328d9073553da4c1e6044f2c171af3d4d0b
yousraamr <yousraamr000@gmail.com>
2025-07-24 02:48:02 +0200

    Update: add home page route

--- lib/utils/custom_router.dart
+++ lib/utils/custom_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'route_names.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/signup_page.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/login_screen.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_pages/home_screen.dart';
 
 class CustomRouter {
   static Route<dynamic>? allRoutes(RouteSettings settings) {
@@ -12,6 +13,9 @@ class CustomRouter {
       case loginScreen:
         return MaterialPageRoute(
             settings: settings, builder: (_) => const SignInScreen());
+      case homeScreen:
+        return MaterialPageRoute(
+            settings: settings, builder: (_) => const HomeScreen());
       default:
         return null;
     }
