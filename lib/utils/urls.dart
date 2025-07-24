a9761eb1902304788531ecf2cf862f4f587277ee
yousraamr <yousraamr000@gmail.com>
2025-07-24 20:08:29 +0200

    implement urls with baseUrl

---
+++ lib/utils/urls.dart
@@ -0,0 +1,5 @@
+class ApiUrls {
+  static const String baseUrl = "https://cartverse-data.onrender.com";
+  static const String login = "$baseUrl/login";
+  static const String register = "$baseUrl/register";
+}
