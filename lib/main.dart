713447a8b16b4555e4dcaab51f4cfd716a44e151
yousraamr <yousraamr000@gmail.com>
2025-07-24 21:08:22 +0200

    Update: main.dart logic and dependency changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:provider/provider.dart';
+import 'package:dio/dio.dart';
+
+import 'package:fedis_mockup_demo/auth/presentation/view_model/auth_provider.dart';
+import 'package:fedis_mockup_demo/auth/data/datasource/auth_datasourse.dart';
+import 'package:fedis_mockup_demo/auth/data/repository/auth_repository_impl.dart';
+import 'package:fedis_mockup_demo/auth/domain/usecases/login_usecase.dart';
+import 'package:fedis_mockup_demo/auth/domain/usecases/register_usecase.dart';
 import 'package:fedis_mockup_demo/themes/theme.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/welcome_page.dart';
 
@@ -7,23 +15,45 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
+  // Dependency Injection
+  final dio = Dio();
+  final dataSource = AuthDataSource(dio);
+  final authRepository = AuthRepositoryImpl(dataSource);
+  final loginUseCase = LoginUseCase(authRepository);
+  final registerUseCase = RegisterUseCase(authRepository);
+
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      path: 'lib/translations',
+      supportedLocales: const [Locale('en')],
+      path: 'assets/translations',
       fallbackLocale: const Locale('en'),
-      child: MyApp(),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => AuthProvider(
+              loginUseCase: loginUseCase,
+              registerUseCase: registerUseCase,
+            ),
+          ),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: lightMode,
-      darkTheme: darkMode,
+      title: 'Fedis Mockup Demo',
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme
+      ),
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
