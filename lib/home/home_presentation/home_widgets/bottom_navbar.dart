a3432b898cecff9b8ca750b78549e17c8a26128f
yousraamr <yousraamr000@gmail.com>
2025-07-25 14:22:22 +0200

    Feat: create bottom navbar

--- lib/home/home_presentation/home_widgets/bottom_navbar.dart
+++ lib/home/home_presentation/home_widgets/bottom_navbar.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:easy_localization/easy_localization.dart';
+import '../home_view_model/nav_provider.dart';
 
 class BottomNavBar extends StatelessWidget {
-  final int currentIndex;
-  final ValueChanged<int> onTap;
-
-  const BottomNavBar({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-      selectedItemColor: Theme.of(context).colorScheme.primary,
-      unselectedItemColor: Theme.of(context).colorScheme.outlineVariant,
-      currentIndex: currentIndex,
-      onTap: onTap,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorites"),
-        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
-      ],
+    return Consumer<NavProvider>(
+      builder: (context, navProvider, _) {
+        return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Theme.of(context).colorScheme.onPrimary,
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Theme.of(context).colorScheme.outlineVariant,
+          currentIndex: navProvider.currentIndex,
+          onTap: navProvider.changeTab,
+          items: [
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.home_outlined), label: 'home'.tr()),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.shopping_cart_outlined),
+                label: 'cart'.tr()),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.favorite_border),
+                label: 'favorites'.tr()),
+            BottomNavigationBarItem(
+                icon: const Icon(Icons.person_outline), label: 'profile'.tr()),
+          ],
+        );
+      },
     );
   }
 }
