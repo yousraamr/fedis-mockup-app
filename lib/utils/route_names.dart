68fe8c41be67468a4b54a9c3026678869e6c7050
yousraamr <yousraamr000@gmail.com>
2025-07-25 14:26:42 +0200

    Update: add profile, cart and fav screens

--- lib/utils/route_names.dart
+++ lib/utils/route_names.dart
@@ -1,3 +1,6 @@
 const registerScreen = 'SignUpScreen';
 const loginScreen = 'SignInScreen';
 const homeScreen = 'HomeScreen';
+const profileScreen = 'ProfileScreen';
+const cartScreen = 'CartScreen';
+const favoritesScreen = 'FavoritesScreen';
