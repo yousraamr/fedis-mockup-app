6dce7d79f15abd6a913e183b8a87be9a0dbb14bf
yousraamr <yousraamr000@gmail.com>
2025-07-25 14:28:37 +0200

    Update: add profile, cart and fav routes

--- lib/utils/custom_router.dart
+++ lib/utils/custom_router.dart
@@ -3,6 +3,9 @@ import 'route_names.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/signup_page.dart';
 import 'package:fedis_mockup_demo/auth/presentation/pages/login_screen.dart';
 import 'package:fedis_mockup_demo/home/home_presentation/home_pages/home_screen.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_pages/profile_screen.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_pages/cart_screen.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_pages/fav_screen.dart';
 
 class CustomRouter {
   static Route<dynamic>? allRoutes(RouteSettings settings) {
@@ -16,6 +19,9 @@ class CustomRouter {
       case homeScreen:
         return MaterialPageRoute(
             settings: settings, builder: (_) => const HomeScreen());
+      case profileScreen:
+        return MaterialPageRoute(
+            settings: settings, builder: (_) => const ProfileScreen());
       default:
         return null;
     }
