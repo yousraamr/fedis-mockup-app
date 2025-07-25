6dcaff4d1b11912303db46d2fc17002e34be8df7
yousraamr <yousraamr000@gmail.com>
2025-07-25 14:25:35 +0200

    create bottom navbar strings for localizarion

---
+++ lib/translations/bottom_navbar_strings.dart
@@ -0,0 +1,6 @@
+class BottomNavBarStrings {
+  static const String home = 'home';
+  static const String cart = 'cart';
+  static const String favorites = 'favorites';
+  static const String profile = 'profile';
+}
