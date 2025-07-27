02600f968f452e3d6d7a9338d65066a9fa6d058d
yousraamr <yousraamr000@gmail.com>
2025-07-27 19:06:02 +0200

    Feat: Logger utility

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -1,13 +1,21 @@
+import 'package:flutter/foundation.dart';
+
 class Logger {
   static void info(String message) {
-    print("ℹ️ INFO: $message");
+    if (kDebugMode) {
+      print("ℹ️ INFO: $message");
+    }
   }
 
   static void success(String message) {
-    print("✅ SUCCESS: $message");
+    if (kDebugMode) {
+      print("✅ SUCCESS: $message");
+    }
   }
 
   static void error(String message) {
-    print("❌ ERROR: $message");
+    if (kDebugMode) {
+      print("❌ ERROR: $message");
+    }
   }
 }
