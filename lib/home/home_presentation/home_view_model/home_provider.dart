fcbb4b58ccfa4205591aa933cbec5a4b9739c18f
yousraamr <yousraamr000@gmail.com>
2025-07-27 19:08:00 +0200

    Feat: Add HomeProvider state logic

---
+++ lib/home/home_presentation/home_view_model/home_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:fedis_mockup_demo/home/home_data/models/ServicesDataModel.dart';
+
+import '../../home_data/home_datasource/home_datasource.dart';
+
+
+class HomeProvider with ChangeNotifier {
+  final HomeDataSource homeDataSource;
+
+  HomeProvider({required this.homeDataSource});
+
+  List<ProductDataModel> _services = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  List<ProductDataModel> get services => _services;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> fetchServices() async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      _services = await homeDataSource.readJsonData();
+    } catch (error) {
+      _errorMessage = error.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
