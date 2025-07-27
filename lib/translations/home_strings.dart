460a7471600f46ee0eedeba4e54da8f054bbfdfe
yousraamr <yousraamr000@gmail.com>
2025-07-27 19:09:01 +0200

    Update: Home page translations

---
+++ lib/translations/home_strings.dart
@@ -0,0 +1,10 @@
+class HomeStrings {
+  static const String hiUser = "hi_user";
+  static const String searchHint = "search_hint";
+  static const String getSpecialDiscount = "get_special_discount";
+  static const String upToDiscount = "up_to_discount";
+  static const String categories = "categories";
+  static const String restaurant = "restaurant";
+  static const String showAll = "show_all";
+  static const String pizza = "pizza";
+}
