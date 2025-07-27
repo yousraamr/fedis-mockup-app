d37d0c570fb03025a7bb8abb946ad7d5114bcca7
yousraamr <yousraamr000@gmail.com>
2025-07-27 19:07:39 +0200

    UI: Improve Home Screen design

--- lib/home/home_presentation/home_pages/home_screen.dart
+++ lib/home/home_presentation/home_pages/home_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fedis_mockup_demo/themes/theme.dart';
-import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/bottom_navbar.dart';
-import 'package:fedis_mockup_demo/home/home_presentation/home_view_model/nav_provider.dart';
-import 'package:fedis_mockup_demo/auth/presentation/view_model/auth_provider.dart';
+import 'package:easy_localization/easy_localization.dart';
 
+import '../home_view_model/home_provider.dart';
+import '../home_widgets/food_item_widget.dart';
 import 'cart_screen.dart';
 import 'fav_screen.dart';
 import 'profile_screen.dart';
+import 'package:fedis_mockup_demo/themes/theme.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/bottom_navbar.dart';
 import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/custom_text.dart';
-import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/category_circle.dart';
-import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/food_item_widget.dart';
 import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/card_button.dart';
-
+import 'package:fedis_mockup_demo/home/home_presentation/home_view_model/nav_provider.dart';
+import 'package:fedis_mockup_demo/auth/presentation/view_model/auth_provider.dart';
+import 'package:fedis_mockup_demo/core/project_widgets/language_toggle_button.dart';
+import 'package:fedis_mockup_demo/home/home_presentation/home_widgets/food_item_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,7 +23,6 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final navProvider = Provider.of<NavProvider>(context);
 
-    // Pages for each BottomNavBar item
     final List<Widget> pages = [
       const HomePageContent(),
       const CartScreen(),
@@ -32,7 +33,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
       body: IndexedStack(
-        index: navProvider.currentIndex, // Keeps all pages alive
+        index: navProvider.currentIndex,
         children: pages,
       ),
       bottomNavigationBar: const BottomNavBar(),
@@ -40,8 +41,21 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class HomePageContent extends StatelessWidget {
+class HomePageContent extends StatefulWidget {
   const HomePageContent({super.key});
+
+  @override
+  State<HomePageContent> createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      Provider.of<HomeProvider>(context, listen: false).fetchServices();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,23 +66,58 @@ class HomePageContent extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Hi, ${authProvider.userName ?? 'User'}!",
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: lightColorScheme.shadow,
-            ),
+          // Header
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                tr('hi_user', namedArgs: {'userName': authProvider.userName ?? 'User'}),
+                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: lightColorScheme.shadow,
+                ),
+              ),
+              LanguageToggleButton(
+                iconColor: lightColorScheme.primary,
+              ),
+            ],
           ),
           const SizedBox(height: 10),
-          const CustomText(hint: "Search Your Food or Restaurant"),
+
+          CustomText(hint: tr('search_hint')),
           const SizedBox(height: 15),
+
           _buildPromoBanner(context),
           const SizedBox(height: 20),
-          _buildSectionHeader(context, "Categories"),
-          const SizedBox(height: 5),
-          _buildCategories(),
+
+          _buildSectionHeader(context, tr('services')),
           const SizedBox(height: 10),
-          _buildSectionHeader(context, "Restaurant"),
-          Expanded(child: _buildRestaurantList()),
+
+          Expanded(
+            child: Consumer<HomeProvider>(
+              builder: (context, homeProvider, _) {
+                if (homeProvider.isLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (homeProvider.errorMessage != null) {
+                  return Center(child: Text(homeProvider.errorMessage!));
+                } else if (homeProvider.services.isEmpty) {
+                  return const Center(child: Text("No services available"));
+                } else {
+                  return ListView.separated(
+                    itemCount: homeProvider.services.length,
+                    separatorBuilder: (context, index) => const SizedBox(height: 10),
+                    itemBuilder: (context, index) {
+                      final service = homeProvider.services[index];
+                      return ServiceItemWidget(
+                        title: service.serviceName ?? 'No Name',
+                        description: service.serviceLink ?? 'No Description',
+                        imageUrl: service.serviceImage ?? '',
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
         ],
       ),
     );
@@ -86,13 +135,13 @@ class HomePageContent extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Get Special Discount",
+              Text(tr('get_special_discount'),
                   style: Theme.of(context)
                       .textTheme
                       .headlineLarge!
                       .copyWith(color: lightColorScheme.outlineVariant)),
               const SizedBox(height: 25),
-              Text("up to 75%",
+              Text(tr('up_to_discount', namedArgs: {'percent': '75'}),
                   style: Theme.of(context)
                       .textTheme
                       .headlineLarge!
@@ -103,26 +152,6 @@ class HomePageContent extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-
-  Widget _buildCategories() {
-    return SizedBox(
-      height: 100,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        itemCount: 7,
-        separatorBuilder: (context, index) => const SizedBox(width: 25),
-        itemBuilder: (context, index) => const CategoryCircle(title: "Pizza"),
-      ),
-    );
-  }
-
-  Widget _buildRestaurantList() {
-    return ListView.separated(
-      itemBuilder: (context, index) => const FoodItemWidget(),
-      separatorBuilder: (context, index) => const SizedBox(height: 10),
-      itemCount: 5,
     );
   }
 
@@ -139,7 +168,7 @@ class HomePageContent extends StatelessWidget {
                   .copyWith(color: lightColorScheme.shadow)),
           InkWell(
             onTap: () {},
-            child: Text("Show all",
+            child: Text(tr('show_all'),
                 style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                   color: lightColorScheme.secondary,
                   decoration: TextDecoration.underline,
