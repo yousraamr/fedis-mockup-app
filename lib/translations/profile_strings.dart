c7fd17df2725c911280a326656e012484773a226
yousraamr <yousraamr000@gmail.com>
2025-07-27 19:09:08 +0200

    Update: Profile page translations

---
+++ lib/translations/profile_strings.dart
@@ -0,0 +1,11 @@
+class ProfileStrings {
+  static const String profile = "profile";
+  static const String confirmLogout = "confirm_logout";
+  static const String logoutQuestion = "logout_question";
+  static const String cancel = "cancel";
+  static const String logout = "logout";
+  static const String loggedOutSuccessfully = "logged_out_successfully";
+  static const String logoutFailed = "logout_failed";
+  static const String user = "user";
+  static const String notAvailable = "not_available";
+}
